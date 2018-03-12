d9292c42eba8a51f1ec2c55503a3b5ba634ee00d
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-12 01:03:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  put "   |   |   "
+  puts "   |   |   "
   
-  put "-----------"
+  puts "-----------"
   
-  put "   |   |   "
+  puts "   |   |   "
   
-  put "-----------"
+  puts "-----------"
   
-  put "   |   |   "
+  puts "   |   |   "
  
    
 end
